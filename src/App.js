db778286e8ba512a001d83afc29178516f292318
Nuuradin <nuuradin.sj@gmail.com>
2023-10-26 18:45:22 -0600

    added insertion sort

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-
-import logo from './626Yhwach_reveals.webp';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          SERVE THE ALMIGHTY YHWACH
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=oQRw23tmzAo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ITS BLEACH TIME
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    Array: [],
+    ArraySteps: [],
+    Colors: [],
+    ColorsSteps: [],
+    CurentStep: 0,
+    BarCount: 0,
+    Delay: 100,
+    TheAlgorithm: '',
+    timeouts: [],
+  }
+  render () {
+    return ( <div className='app'>
+      <h1> hello world </h1>
     </div>
-  );
-}
+    );
+  }
+
+}   
+  
 
 export default App;
